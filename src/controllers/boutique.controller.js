5dade4a181d9429cc0ac9dd404d70b32e1be34fc
loicBe21 <loicRavelo05@gmail.com>
2026-02-09 15:13:47 +0100

    fix import

--- src/controllers/boutique.controller.js
+++ src/controllers/boutique.controller.js
@@ -14,6 +14,7 @@ const {
   getBoutiquesInactives,
   activerBoutique,
   desactiverBoutique,
+  getBoutiquesPaginated,
 } = require("../services/boutique.service");
 
 // ============================================
