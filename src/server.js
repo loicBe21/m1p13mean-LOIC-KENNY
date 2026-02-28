d55f3de865269c4300a6cd45e208f1491bc6cab3
Labarre12 <labarreitokiana@gmail.com>
2026-03-01 02:46:53 +0300

    server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,32 @@
-require("./config/env");
+import dotenv from "dotenv";
+dotenv.config({ path: './.env' }); // doit être tout en haut
 
-const app = require("./app");
-const { PORT, NODE_ENV } = require("./config/env");
-const connectDB = require("./config/db");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import auth from "./routes/auth.routes.js";
 
-// Connexion à MongoDB
-connectDB().then(() => {
-  // Démarrage serveur seulement si DB OK
-  app.listen(PORT, () => {
-    console.log(`🚀 API running on port ${PORT} (${NODE_ENV})`);
-  });
-});
+const app = express();
 
+// Parser JSON avant les routes
+app.use(express.json());
+
+// CORS
+app.use(cors({
+  origin: "http://localhost:4200",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+// Routes
+app.use("/api/auth", auth);
+
+// Vérifie si la variable d'environnement est bien chargée
+console.log("Mongo URI:", process.env.MONGODB_URI);
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("✅ MongoDB connecté !"))
+  .catch(err => console.error("❌ MongoDB connection failed:", err));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`🚀 Serveur démarré sur le port ${port}`));
