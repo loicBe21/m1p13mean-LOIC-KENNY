f3d0d49deebe98ede4eaedb5b801658e0e7cd687
Labarre12 <labarreitokiana@gmail.com>
2026-03-02 14:54:48 +0300

    config

--- src/app.js
+++ src/app.js
@@ -15,8 +15,16 @@ const produitRoutes = require("./routes/produit.routes");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+
 app.use(myLoggerMiddelware);
 
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,4 @@
 require("./config/env");
-const express = require("express");
-const cors = require("cors");
-
-const authRoutes = require("./routes/auth.routes");
-const categorieRoutes = require("./routes/categorie.routes");
 
 const app = require("./app");
 const { PORT, NODE_ENV } = require("./config/env");
@@ -16,17 +11,3 @@ connectDB().then(() => {
     console.log(`🚀 API running on port ${PORT} (${NODE_ENV})`);
   });
 });
-
-
-app.use(
-  cors({
-    origin: "http://localhost:4200",
-    credentials: true,
-  })
-);
-
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ extended: true, limit: "100mb" }));
-
-app.use("/api/auth", authRoutes);
-app.use("/api/categories", categorieRoutes);
