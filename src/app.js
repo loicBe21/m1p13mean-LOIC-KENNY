a81accd527b6e3e4e12c3e764e305c6315ab95f2
Labarre12 <labarreitokiana@gmail.com>
2026-03-02 14:58:10 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,12 +15,7 @@ const produitRoutes = require("./routes/produit.routes");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:4200",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
