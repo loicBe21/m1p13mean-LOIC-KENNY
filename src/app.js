bf6e0739ff9fc9d50513691179034bbf5715478a
Kenny1593 <114820530+Kenny1593@users.noreply.github.com>
2026-03-02 14:59:10 +0300

    Set body parser limits to 100mb
    Increase JSON and URL-encoded body size limits to 100mb.

--- src/app.js
+++ src/app.js
@@ -16,7 +16,10 @@ const produitRoutes = require("./routes/produit.routes");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+
 app.use(myLoggerMiddelware);
 
 
